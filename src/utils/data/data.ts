115732b5edcfd7df46bda68908c3574a046e25a9
yuval <wdvwdvwdv6@gmail.com>
2023-06-29 14:46:05 +0300

    merge with main

--- src/utils/data/data.ts
+++ src/utils/data/data.ts
@@ -1,9 +1,9 @@
 import { DogFormData } from '../types/type';
-export const SERVER_URL = 'https://pawtner-server.onrender.com';
-export const WEBSITE_URL =
-  'https://pawtner-client-6q7jscvgt-yuvaltwina.vercel.app';
-// export const WEBSITE_URL = 'http://localhost:5173';
-// export const SERVER_URL = 'http://localhost:3000';
+// export const SERVER_URL = 'https://pawtner-server.onrender.com';
+// export const WEBSITE_URL =
+//   'https://pawtner-client-6q7jscvgt-yuvaltwina.vercel.app';
+export const WEBSITE_URL = 'http://localhost:5173';
+export const SERVER_URL = 'http://localhost:3000';
 export const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/; // atleast 8 characters , must one letter, must one number
 export const EMAIL_REGEX = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 export const EMAIL_ERROR_MESSAGE = 'please provide a valid Email address';
